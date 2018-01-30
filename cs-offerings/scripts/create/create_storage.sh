ffe81ae7f4387e6d84f53587972e74f4a0bd17e9
dhyey <dbshah@ibm.com>
2018-01-30 17:41:05 +0530

    adding new pvcs to create and delete storage scripts

--- cs-offerings/scripts/create/create_storage.sh
+++ cs-offerings/scripts/create/create_storage.sh
@@ -9,10 +9,15 @@ else
 fi
 
 PAID=false
+WITH_COUCHDB=false
 
 Parse_Arguments() {
 	while [ $# -gt 0 ]; do
 		case $1 in
+			--with-couchdb)
+				echo "Configured to setup network with couchdb"
+				WITH_COUCHDB=true
+				;;
 			--paid)
 				echo "Configured to setup a paid storage on ibm-cs"
 				PAID=true
@@ -32,9 +37,9 @@ fi
 
 echo "Creating Persistent Volumes"
 if [ "${PAID}" == "true" ]; then
-	if [ "$(kubectl get pvc | grep shared-pvc | awk '{ print $2 }')" != "Bound" ] || [ "$(kubectl get pvc | grep composer-pvc | awk '{ print $2 }')" != "Bound" ]; then
+	if [ "$(kubectl get pvc | grep shared-pvc | awk '{ print $2 }')" != "Bound" ] || [ "$(kubectl get pvc | grep composer-pvc | awk '{ print $2 }')" != "Bound" ] || [ "$(kubectl get pvc | grep ledger-pvc | awk '{ print $2 }')" != "Bound" ]; then
 		echo "The paid PVC does not seem to exist"
-		echo "Creating PVC named shared-pvc and composer-pvc"
+		echo "Creating PVC named shared-pvc, composer-pvc and ledger-pvc"
 
 		# making a PVC on ibm-cs paid version
 		echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/storage-paid.yaml"
@@ -52,9 +57,42 @@ if [ "${PAID}" == "true" ]; then
 			echo "Waiting for composer-pvc to be bound"
 			sleep 5
 		done
+
+		while [ "$(kubectl get pvc | grep ledger-pvc | awk '{print $2 }')" != "Bound" ];
+		do
+			echo "Waiting for ledger-pvc to be bound"
+			sleep 5
+		done
 	else
-		echo "The PVC with name shared-pvc or composer-pvc exists, not creating again"
+		echo "The PVC with name shared-pvc or composer-pvc or ledger-pvc exists, not creating again"
 		#echo "Note: This can be a normal storage and not a ibm-cs storage, please check for more details"
+	fi
+
+	if [ "${WITH_COUCHDB}" == "true" ]; then
+		echo "Couchdb is enabled creating storage for couchdb"
+		if [ "$(kubectl get pvc | grep couchdb1-pvc | awk '{ print $2 }')" != "Bound" ] || [ "$(kubectl get pvc | grep couchdb2-pvc | awk '{ print $2 }')" != "Bound" ]; then
+			echo "The paid PVC for couchdb does not seem to exist"
+			echo "Creating PVC named couchdb1 and couchdb2"
+		fi
+
+		# making a PVC on ibm-cs paid version
+		echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/storage-couchdb-paid.yaml"
+		kubectl create -f ${KUBECONFIG_FOLDER}/storage-couchdb-paid.yaml
+		sleep 5
+
+		while [ "$(kubectl get pvc | grep couchdb1-pvc | awk '{print $2 }')" != "Bound" ];
+		do
+			echo "Waiting for couchdb1-pvc to be bound"
+			sleep 5
+		done
+
+		while [ "$(kubectl get pvc | grep couchdb2-pvc | awk '{print $2 }')" != "Bound" ];
+		do
+			echo "Waiting for couchdb2-pvc to be bound"
+			sleep 5
+		done
+	else
+		echo "The PVC with name couchdb1-pvc or couchdb2-pvc exists, not creating again"
 	fi
 else
 	if [ "$(kubectl get pvc | grep shared-pvc | awk '{print $2}')" != "Bound" ]; then
@@ -74,5 +112,30 @@ else
 		echo "The Persistant Volume exists, not creating again"
 	fi
 
+	if [ "${WITH_COUCHDB}" == "true" ]; then
+		echo "Couchdb is enabled creating storage for couchdb"
+		if [ "$(kubectl get pvc | grep couchdb1-pvc | awk '{ print $2 }')" != "Bound" ] || [ "$(kubectl get pvc | grep couchdb2-pvc | awk '{ print $2 }')" != "Bound" ]; then
+			echo "The PVC for couchdb does not seem to exist or is not bound"
+			echo "Creating PVC named couchdb1 and couchdb2"
+
+			echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/storage-couchdb-free.yaml"
+			kubectl create -f ${KUBECONFIG_FOLDER}/storage-couchdb-free.yaml
+			sleep 5
+
+			while [ "$(kubectl get pvc | grep couchdb1-pvc | awk '{print $2 }')" != "Bound" ];
+			do
+				echo "Waiting for couchdb1-pvc to be bound"
+				sleep 5
+			done
+
+			while [ "$(kubectl get pvc | grep couchdb2-pvc | awk '{print $2 }')" != "Bound" ];
+			do
+				echo "Waiting for couchdb2-pvc to be bound"
+				sleep 5
+			done
+		else
+			echo "The PVC with name couchdb1-pvc or couchdb2-pvc exists, not creating again"
+		fi
+	fi
 fi
 
